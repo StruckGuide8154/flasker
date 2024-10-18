fc0c30d945b239908cf2d13584259588d0ff40c7
StruckGuide8154 <109350690+StruckGuide8154@users.noreply.github.com>
2024-10-18 21:46:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash, current_app
+import flask
+from flask import Flask, render_template, request, redirect, url_for, flash, current_app, flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
